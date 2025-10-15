3096019f34d1dacc79369c4ddfaf8700fa8800c1
Ragazzi147 <lucasax147@gmail.com>
2025-10-15 15:46:56 -0300

    Estrutura inicial

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import Login from './Components/Login/Login';
+import Home from './Components/Home';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
